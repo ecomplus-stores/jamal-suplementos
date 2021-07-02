95a1513585f39b77d2ecb49de4f9ece396ec971d
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-07-02 13:03:53 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,2 @@
 // Add your custom JavaScript for storefront pages here.
+if (location.pathname !== '/pages/jamal-suplementos-distribuidora') {location.href = 'https://www.jamalsuplementos.com.br/pages/jamal-suplementos-distribuidora'}
